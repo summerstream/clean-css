af17561a9d76bbf218dec2114f8d2f0b6c09bb60
rrchang <rrchang@Ctrip.com>
2017-03-30 14:25:27 +0800

    package

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var path = require('path');
 var file = require('file-system');
 var util = require('utils-extend');
 
+var fileContent = file.readFileSync(path.join(__dirname, 'unused_css.txt'));
+
+console.log(fileContent);
