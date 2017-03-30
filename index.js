e667d9b3d7b8aff51a1e79959b30ad2e60885c60
rrchang <rrchang@Ctrip.com>
2017-03-30 15:29:57 +0800

    line reader

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var path = require('path');
 var file = require('file-system');
 var util = require('utils-extend');
+var fs = require('fs');
 
-var fileContent = file.readFileSync(path.join(__dirname, 'unused_css.txt'));
+var css = file.readFileSync(path.join(__dirname, 'isd1.css'),{ encoding: 'utf8' });
 
-console.log(fileContent);
+console.log(css);
+
+// var lineReader = require('readline').createInterface({
+//   input: require('fs').createReadStream(path.join(__dirname, 'unused_css.txt'))
+// });
+
+// lineReader.on('line', function (line) {
+//   console.log('Line from file:', line);
+  
+// });
+
+// file.fs.writeFileSync(filepath, contents, { encoding: 'utf8' });
