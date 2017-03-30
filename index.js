3b6275ad3c8e8e4f2c168351e2fecdf838c87fee
rrchang <rrchang@Ctrip.com>
2017-03-30 15:32:36 +0800

    formate css

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ var css = file.readFileSync(path.join(__dirname, 'isd1.css'),{ encoding: 'utf8' 
 
 console.log(css);
 
-// var lineReader = require('readline').createInterface({
-//   input: require('fs').createReadStream(path.join(__dirname, 'unused_css.txt'))
-// });
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(path.join(__dirname, 'unused_css.txt'))
+});
 
-// lineReader.on('line', function (line) {
-//   console.log('Line from file:', line);
-  
-// });
+lineReader.on('line', function (line) {
+  console.log('Line from file:', line);
+  var regexString = 
+});
+
+console.log(css);
 
 // file.fs.writeFileSync(filepath, contents, { encoding: 'utf8' });
