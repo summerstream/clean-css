e22fd383ce26da7027ca0d2580851b65bf10c24a
culnoty <culnoty@outlook.com>
2017-03-30 23:08:43 +0800

    u

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ var fs = require('fs');
 
 var contents = file.readFileSync(path.join(__dirname, 'isd1.css'),{ encoding: 'utf8' });
 
-console.log(contents);
+// console.log(contents);
 
 var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream(path.join(__dirname, 'unused_css.txt'))
 });
 
 lineReader.on('line', function (line) {
-  line = line.replace(/\./g,'\\.');
+  line = line.replace(/\./g,'\\.').replace(/\[/g,'\\[').replace(/\]/g,'\\]');
   console.log('Line from file:', line);
-  var reg = new RegExp(line + '[\s]*{[\r\n\w\s\b:\-%"\\;]*}');
+  var reg = new RegExp(line + '[\\s]*{[\\r\\n\\w\\s\\b:\\-%"\\\\;]*}','g');
   contents = contents.replace(reg, 'hahahahahaha');
 
 });
 
-console.log(contents);
+// console.log(contents);
 
 // file.fs.writeFileSync(filepath, contents, { encoding: 'utf8' });
